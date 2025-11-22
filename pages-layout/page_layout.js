26c21f088a7aaf5d470bbf2e4d46dbddfb4e5eba
Stefanijaaaa <163347673+Stefanijaaaa@users.noreply.github.com>
2025-11-22 17:03:46 +0100

    Add files via upload
    Add page layout files
    
    - Created place detail page template
    - Includes map integration, photo gallery, reviews section
    - Shows login prompt for non-authenticated users

---
+++ pages-layout/page_layout.js
@@ -0,0 +1,19 @@
+function showLoginModal() {
+    document.getElementById('loginModal').classList.add('show');
+}
+    
+function hideLoginModal() {
+    document.getElementById('loginModal').classList.remove('show');
+}
+
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+    .openPopup();
+
+// https://leafletjs.com/examples/quick-start/ -> tutorial for the map, will need to add exact locations 
